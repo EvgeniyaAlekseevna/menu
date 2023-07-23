1a31670b6c3eb7f5054c2e99860fdfc348d4caac
EvgeniyaAlekseevna <zhenya.sudniczina@yandex.ru>
2023-07-23 20:53:50 +0300

    Initial commit

---
+++ src/schemas.py
@@ -0,0 +1,29 @@
+from uuid import UUID
+from pydantic import BaseModel
+
+
+class MenuSchemaBase(BaseModel):
+    title: str
+    description: str
+
+    class Config:
+        orm_mode = True
+
+
+class MenuSchema(MenuSchemaBase):
+    id: UUID
+    submenus_count: int
+    dishes_count: int
+
+
+class SubmenuSchema(MenuSchemaBase):
+    id: UUID
+    dishes_count: int
+
+
+class DishSchemaBase(MenuSchemaBase):
+    price: str
+
+
+class DishSchema(DishSchemaBase):
+    id: UUID
